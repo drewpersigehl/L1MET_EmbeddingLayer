5fa0e2eb72024612bb5c210f35faedcf96fe64d7
drewpersigehl <82611511+drewpersigehl@users.noreply.github.com>
2021-08-17 18:47:50 -0500

    Change example weights

--- embed-test.cpp
+++ embed-test.cpp
@@ -15,7 +15,16 @@ int main() {
 
 	int data;
 	float res[n_out];
-	float weights[n_in*n_out];
+	float weights[n_in*n_out] = {0.69405118, 0.85314349, 0.44348042, 0.98682741, 0.07212228, 0.44294918,
+			  0.50451567, 0.89471736,
+			 0.5172397,  0.73553228, 0.32378551, 0.08363885, 0.90106569, 0.34001472,
+			  0.95520333, 0.09582401,
+			 0.37217257, 0.33020137, 0.58489308, 0.59285919, 0.99916981, 0.28807132,
+			  0.47801168, 0.94389581,
+			 0.81038834, 0.34495702, 0.87173576, 0.36079162, 0.8432279,  0.74681984,
+			  0.96527759, 0.83150219,
+			 0.85617576, 0.117599, 0.63892696, 0.12100261, 0.83511491, 0.0326155,
+			  0.18490572, 0.55708856};
 
 	for (int test = 1; test <= NTEST; ++test) {
 
@@ -25,13 +34,6 @@ int main() {
 		// initialize the result vector as zeros
 		for (int i = 0; i < n_out; i++) {
 			res[i] = 0;
-		}
-
-		// initialize the weights vector arbitrarily
-		for (int i = 0; i < n_in; i++) {
-			for (int j = 0; j < n_out; j++) {
-				weights[i*n_out + j] = i;  // such that row x should look like {x, x, ...}
-			}
 		}
 
 		embed(data, res, weights);
