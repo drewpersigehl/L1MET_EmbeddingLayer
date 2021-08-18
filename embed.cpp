d4031889c46b1e92ce5b221d33fea551104db14f
drewpersigehl <82611511+drewpersigehl@users.noreply.github.com>
2021-08-17 20:05:33 -0500

    added first HLS optimizations

--- embed.cpp
+++ embed.cpp
@@ -5,8 +5,11 @@ void embed(
 	float res[n_out],
 	float weights[n_in*n_out])
 {
+	#pragma HLS ARRAY_PARTITION variable=weights complete
+
 	// copy over the corresponding row in the weights lookup table
 	for (int i = 0; i < n_out; i++) {
+		#pragma unroll
 		res[i] = weights[data * n_out + i];
 	}
 }
